ddc542e0490206269da74bfe84530a8f743c91b8
«roki» <boboevhadiyatullo55@gmail.com>
2024-11-22 00:11:42 +0700

    test starting

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("               Рубаи                ");
+        System.out.println("Много лет размышля я наад жизнью земной.");
+        System.out.println("Непонятного нет для меня под луной.");
+        System.out.println("Мне известно, что мнеп ничего не известно!");
+        System.out.println("Вот последняя правда, открытая мной.");
+        System.out.println("                                О. Хайям");
+    }
+}
